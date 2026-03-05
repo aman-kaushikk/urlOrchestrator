19e57fdcfdf639c67dae17983ceca15c46ea05fc
amankaushik <amankaushikmuz@gmail.com>
2026-03-05 10:35:50 +0530

    initial spring boot project urlOrchestrator

---
+++ src/main/java/io/github/aman/urlorchestrator/UrlOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.aman.urlorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlOrchestratorApplication.class, args);
+    }
+
+}
