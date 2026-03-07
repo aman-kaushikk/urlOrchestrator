009cd05b8738c0205702d0721740994a6e4b3e7d
amankaushik <amankaushikmuz@gmail.com>
2026-03-08 00:23:31 +0530

    test: Gradle boot test added, service method name change

--- src/test/java/io/github/aman/urlorchestrator/UrlOrchestratorApplicationTests.java
+++ src/test/java/io/github/aman/urlorchestrator/UrlOrchestratorApplicationTests.java
@@ -1,13 +1,16 @@
 package io.github.aman.urlorchestrator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class UrlOrchestratorApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("Running url orchestrator application test ...........");
     }
 
 }
---
+++ src/test/java/io/github/aman/urlorchestrator/api/UrlControllerTest.java
@@ -0,0 +1,86 @@
+package io.github.aman.urlorchestrator.api;
+
+import io.github.aman.urlorchestrator.dto.ShortenRequest;
+import io.github.aman.urlorchestrator.service.UrlService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UrlController.class)
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL) // enable constructor dependency injection
+@Slf4j
+@RequiredArgsConstructor
+class UrlControllerTest {
+    private final MockMvc mockMvc;
+    private final ObjectMapper objectMapper;
+
+    @MockitoBean
+    private UrlService urlService;
+
+    @Test
+    void shouldAcceptValidUrl() throws Exception {
+        //prepare
+        var shortenRequest = new ShortenRequest("https://example.com/very/long/path");
+        var body = objectMapper.writeValueAsString(shortenRequest);
+        var expectedValue = "my-short-url-response";
+        when(urlService.resolvedUrl(any())).thenReturn(expectedValue);
+
+        // perform
+        mockMvc.perform(post("/api/v1/shorten")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.shortenCode").exists())
+                .andExpect(jsonPath("$.shortenCode").value(expectedValue));
+    }
+
+    @Test
+    void shouldReturnValidationErrorForInvalidLongUrl() throws Exception {
+        //prepare
+        var shortenRequest = new ShortenRequest("ftp://example.com");
+        var body = objectMapper.writeValueAsString(shortenRequest);
+
+        mockMvc.perform(post("/api/v1/shorten")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body))
+                // status
+                .andExpect(status().isBadRequest())
+
+                // top-level fields
+                .andExpect(jsonPath("$.timestamp").exists())
+                .andExpect(jsonPath("$.message").value("Validation failed"))
+
+                // error array
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors.length()").value(1))
+
+                // error details
+                .andExpect(jsonPath("$.errors[0].field").value("longUrl"))
+                .andExpect(jsonPath("$.errors[0].error")
+                        .value("longUrl must be a valid http or https URL"));
+    }
+
+    @Test
+    void shouldRejectBlankUrl() throws Exception {
+        //prepare
+        var shortenRequest = new ShortenRequest("");
+        var body = objectMapper.writeValueAsString(shortenRequest);
+
+        mockMvc.perform(post("/api/v1/shorten")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body))
+                .andExpect(status().isBadRequest());
+    }
+}
