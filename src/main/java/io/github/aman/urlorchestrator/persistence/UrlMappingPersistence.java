b5e863815575593965fba20b12fdc0e0dc10ed87
amankaushik <amankaushikmuz@gmail.com>
2026-03-11 08:43:17 +0530

    refact:rem commit

--- src/main/java/io/github/aman/urlorchestrator/persistence/UrlMappingPersistence.java
+++ src/main/java/io/github/aman/urlorchestrator/persistence/UrlMappingPersistence.java
@@ -1,4 +1,25 @@
 package io.github.aman.urlorchestrator.persistence;
 
+import io.github.aman.urlorchestrator.entity.UrlMapping;
+import io.github.aman.urlorchestrator.repo.UrlRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class UrlMappingPersistence {
+    private final UrlRepository repository;
+    public UrlMapping save(String shortCode, String longUrl) {
+        UrlMapping mapping = new UrlMapping();
+        mapping.setShortCode(shortCode);
+        mapping.setLongUrl(longUrl);
+        UrlMapping saved = repository.save(mapping);
+
+        log.debug("Persisted UrlMapping [id={}, shortCode={}]", saved.getId(), saved.getShortCode());
+
+        return saved;
+    }
+
 }
--- src/main/java/io/github/aman/urlorchestrator/service/UrlService.java
+++ src/main/java/io/github/aman/urlorchestrator/service/UrlService.java
@@ -1,29 +1,37 @@
 package io.github.aman.urlorchestrator.service;
 
-import io.github.aman.urlorchestrator.repo.UrlRepository;
+import io.github.aman.urlorchestrator.persistence.UrlMappingPersistence;
 import io.github.aman.urlorchestrator.utility.Base62;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class UrlService {
 
-//    public String shorten(String longUrl) {
-//        return longUrl;
-//    }
-    private final UrlRepository urlRepository;
-    private final SnowflakeIdGenerator idGenerator;
 
+    private final UrlMappingPersistence presistence;
+    private final SnowflakeIdGenerator idGenerator;
+    private final RedisTemplate<String,String> redisTemplate;
+
+    @Transactional
     public String generatedShortenCode(String longUrl) {
         log.info("Generating snowflake id for url: {}", longUrl);
         final var snowflakeId = idGenerator.nextId();
         final var shortenCode = Base62.encode(snowflakeId);
         log.info("Generated Snowflake id: {}", snowflakeId);
         log.info("Generated shorten code: {}", shortenCode);
-        return shortenCode;
+        var savedUrlMapping = presistence.save(shortenCode, longUrl);
+        // Cache the mapping in Redis
+        redisTemplate.opsForValue().set(shortenCode, longUrl);
+        log.info("Cached in Redis - Code: {}, URL: {}", shortenCode, longUrl);
+
+        return savedUrlMapping.getShortCode();
     }
 
     public String resolvedUrl(String code) {
